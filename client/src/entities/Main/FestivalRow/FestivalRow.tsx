4e0fb62d89fce8b30abd634e62f24ccdca57d301
Egor Zhdanov <egorzhdanov.js@mail.ru>
2025-05-19 13:19:26 +0700

    Small fixes to festival TEST

--- client/src/entities/Main/FestivalRow/FestivalRow.tsx
+++ client/src/entities/Main/FestivalRow/FestivalRow.tsx
@@ -4,24 +4,13 @@ import { useNavigate } from 'react-router-dom';
 import { FESTIVALS_ROUTE, FESTIVAL_ROUTE } from '../../../shared/lib/utils/consts';
 import s from './FestivalRow.module.css';
 import TitleWithUnderline from '../../../shared/ui/TitleWithUnderline/TitleWithUnderline';
-import { useEffect, useState } from 'react';
 
 const FestivalRow = observer(() => {
   const { store } = useStore();
   const visibleFestivals = 4;
   const navigate = useNavigate();
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
 
   const dateFormatter = new Intl.DateTimeFormat('ru-RU', { day: 'numeric', month: 'long' });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 600);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   return (
     <>
@@ -32,30 +21,30 @@ const FestivalRow = observer(() => {
           <span className={s.festivalDate}>
             {dateFormatter.format(new Date(festival.date))}
           </span>
-          <div className={s.withoutDate}>
-            <img
-              alt="Фестивальная картинка"
-              className={s.festivalImg}
-              src={import.meta.env.VITE_API_URL + festival.img}
+          <img
+            alt="Фестивальная картинка"
+            className={s.festivalImg}
+            src={import.meta.env.VITE_API_URL + festival.img}
+            onClick={() => navigate(FESTIVAL_ROUTE + '/' + festival.id)}
+          />
+          <div>
+            <h2
+              className={s.festivalTitle}
               onClick={() => navigate(FESTIVAL_ROUTE + '/' + festival.id)}
-            />
-            <div>
-              <h2
-                className={s.festivalTitle}
-                onClick={() => navigate(FESTIVAL_ROUTE + '/' + festival.id)}
-              >
-                {festival.name}
-              </h2>
-            </div>
+            >
+              {festival.name}
+            </h2>
           </div>
         </div>
       ))}
+      {visibleFestivals < store.festivals.length && (
         <button 
           className={s.buttonStyle}
           onClick={() => navigate(FESTIVALS_ROUTE)}
         >
-          {!isMobile ? 'Еще' : 'Посмотреть все фестивали'}
+          Еще
         </button>
+      )}
       </div>
     </>
   );
