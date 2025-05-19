20c0049ada00a027ee000b6e188b78e26c52d6dd
Egor Zhdanov <egorzhdanov.js@mail.ru>
2025-05-19 13:32:23 +0700

    Soft changes

--- client/src/entities/modals/CreateActor.tsx
+++ client/src/entities/modals/CreateActor.tsx
@@ -75,7 +75,7 @@ const CreateActor = observer(({ show, onHide }: ModalProps) => {
                 value={town}
                 onChange={e => setTown(e.target.value)}
                 className={s.inputField}
-                placeholder="Введите город рождения"
+                placeholder="Введите город"
                 type="text"
             />
             <input
